454f154e909d7d402460f429146ae43139fca91f
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2023-06-26 16:03:19 -0400

    Fixed plugin imports

--- web page/src/index.js
+++ web page/src/index.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import pushkinClient from 'pushkin-client';
-import jsPsych from 'pushkin-jspsych';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import timeline_basic from './experiment';
-import jsYaml from 'js-yaml';
-const fs = require('fs');
+import React from "react";
+import pushkinClient from "pushkin-client";
+import jsPsych from "pushkin-jspsych";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import timeline_basic from "./experiment";
+import jsYaml from "js-yaml";
+const fs = require("fs");
 
 //stylin'
-import './assets/experiment.css'
+import "./assets/experiment.css";
 
-const expConfig = jsYaml.load(fs.readFileSync('../config.yaml'), 'utf8');
+const expConfig = jsYaml.load(fs.readFileSync("../config.yaml"), "utf8");
 
 const pushkin = new pushkinClient();
 window.jsPsych = jsPsych;
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    userID: state.userInfo.userID
+    userID: state.userInfo.userID,
   };
-}
+};
 
 class quizComponent extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { loading: true };
@@ -36,31 +34,36 @@ class quizComponent extends React.Component {
   async startExperiment() {
     this.setState({ experimentStarted: true });
 
-    jsPsych.data.addProperties({user_id: this.props.userID}); //See https://www.jspsych.org/core_library/jspsych-data/#jspsychdataaddproperties
+    jsPsych.data.addProperties({ user_id: this.props.userID }); //See https://www.jspsych.org/core_library/jspsych-data/#jspsychdataaddproperties
     await pushkin.connect(this.props.api);
     await pushkin.prepExperimentRun(this.props.userID);
     await pushkin.loadScripts([
-      'https://cdn.jsdelivr.net/gh/jspsych/jsPsych@6.0.4/plugins/jspsych-html-keyboard-response.js',
+      "https://cdn.jsdelivr.net/gh/jspsych/jsPsych@6.0.4/plugins/jspsych-html-keyboard-response.js",
+      "https://cdn.jsdelivr.net/gh/jspsych/jsPsych@6.0.4/plugins/jspsych-html-button-response.js",
+      "https://cdn.jsdelivr.net/gh/jspsych/jsPsych@6.0.4/plugins/jspsych-survey-text.js",
     ]);
     const timeline = pushkin.setSaveAfterEachStimulus(timeline_basic);
     await jsPsych.init({
-      display_element: document.getElementById('jsPsychTarget'),
+      display_element: document.getElementById("jsPsychTarget"),
       timeline: timeline,
       on_finish: this.endExperiment.bind(this),
     });
 
-    document.getElementById('jsPsychTarget').focus();
+    document.getElementById("jsPsychTarget").focus();
     this.setState({ loading: false });
   }
 
   async endExperiment() {
     document.getElementById("jsPsychTarget").innerHTML = "Processing...";
-    await pushkin.tabulateAndPostResults(this.props.userID, expConfig.experimentName)
-    document.getElementById("jsPsychTarget").innerHTML = "Thank you for participating!";
+    await pushkin.tabulateAndPostResults(
+      this.props.userID,
+      expConfig.experimentName
+    );
+    document.getElementById("jsPsychTarget").innerHTML =
+      "Thank you for participating!";
   }
 
   render() {
-
     return (
       <div>
         {this.state.loading && <h1>Loading...</h1>}
