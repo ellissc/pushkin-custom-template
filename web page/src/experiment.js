dd66556e0a72780fbbb9d15ffb90c01870952878
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2023-06-26 15:48:25 -0400

    Added separate js files for stimuli and consent

--- web page/src/experiment.js
+++ web page/src/experiment.js
@@ -1,13 +1,57 @@
-import jsPsych from 'pushkin-jspsych';
+import jsPsych from "pushkin-jspsych";
+const stimArray = require("./stim").default;
+const consent = require("./consent.js").default;
 
-const timeline = []
+const timeline = [];
 
+// Read in the data.
+// Placeholder.
 
-var hello_trial = {
-    type: 'html-keyboard-response',
-    stimulus: 'Hello world!'
-}
+// Preamble / Consent
 
-timeline.push(hello_trial);
+var welcome = {
+  type: "html-button-response",
+  stimulus: consent + "<br/><p>Press the button to continue.</p>",
+  choices: ["Continue"],
+};
+
+timeline.push(welcome);
+
+// Instructions
+
+var instructions = {
+  type: "html-button-response",
+  stimulus: `
+          <h1>Instructions</h1>
+          <p>In this study, you will be asked to complete a variety of tasks.</p>
+          <p>In the first... In the second... In the third...</p>
+        `,
+  choices: ["Continue"],
+};
+timeline.push(instructions);
+
+var trial = {
+  timeline: [
+    {
+      type: "html-keyboard-response",
+      stimulus:
+        "<div>Press spacebar when you are ready to read some text.</div>",
+    },
+    {
+      type: "survey-text",
+      preamble:
+        "Please provide the first three words that come to mind: " +
+        jsPsych.timelineVariable("word1"),
+      questions: [
+        { prompt: "", placeholder: "First word" },
+        { prompt: "", placeholder: "Second word" },
+        { prompt: "", placeholder: "Third word" },
+      ],
+    },
+  ],
+  timeline_variables: stimArray,
+};
+
+timeline.push(trial);
 
 export default timeline;
---
+++ web page/src/stim.js
@@ -0,0 +1,10 @@
+// Example stimuli
+
+const stimArray = [
+  { word1: "Cat" },
+  { word1: "Broadcast" },
+  { word1: "Torrent" },
+  { word1: "Icon" },
+];
+
+export default stimArray;
