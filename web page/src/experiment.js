b975899dc8c7074573e3ea194ba6dbd76a8cdbe3
Adam Yang <Adamyang2002@gmail.com>
2023-06-27 13:54:23 -0400

    Update experiment.js

--- web page/src/experiment.js
+++ web page/src/experiment.js
@@ -1,4 +1,5 @@
 import jsPsych from "pushkin-jspsych";
+
 const stimArray = require("./stim.js").default;
 const consent = require("./consent.js").default;
 
@@ -14,7 +15,7 @@ const timeline = [];
 
 var welcome = {
   type: "html-button-response",
-  stimulus: consent + "<br/><p>Press the button to continue.</p>",
+  stimulus: "<div id = 'welcome-text'>" + consent + "<br/><p>Press the button to continue.</p>" + "</div>,
   choices: ["Continue"],
 };
 
@@ -22,8 +23,7 @@ timeline.push(welcome);
 
 var welcome2 = {
   type: "html-button-response",
-  stimulus:
-    "<p>Welcome to the language age quiz! You'll provide some word associations.</p>",
+  stimulus:"<div id = 'welcome-text'><p>Welcome to the language age quiz!</p></div>",
   choices: ["Continue"],
 };
 
@@ -34,33 +34,22 @@ timeline.push(welcome2);
 var instructions = {
   type: "html-button-response",
   stimulus: `
+        <div id = 'welcome-text'>
          <h1>Instructions</h1>
           <p>In this quiz, you will be asked to provide the first three words that come to mind for a cue!</p>
           <p>Try to respond as quickly as you can. You do not need to fill all three boxes.</p>
           </br>
           <p>At the end of the quiz, we will use <b>advanced space-age artificial intelligence</b> guess your age based on your answers!</p>
+        </div>
         `,
   choices: ["Continue"],
 };
 timeline.push(instructions);
 
-// var trial = {
-//   timeline: [
-//     {
-//       type: "survey-text",
-//       preamble: () => jsPsych.timelineVariable("word1"),
-//       questions: [{ prompt: "" }, { prompt: "" }, { prompt: "" }],
-//     },
-//   ],
-//   timeline_variables: stimArray,
-// };
-
-// timeline.push(trial);
-
 var example_trial = {
   type: "survey-text",
   preamble:
-    "\n\n\nHere is an example: 'potato'. \nPlease enter the first three words that come to mind.",
+    "\n\n\nHere is an example: <p id = 'cue-words'>potato</p>. \nPlease enter the first three words that come to mind.",
   questions: [{ prompt: "" }, { prompt: "" }, { prompt: "" }],
 };
 
@@ -77,7 +66,7 @@ timeline.push(feedback);
 for (var i = 0; i < stimArray.length; i++) {
   var trial = {
     type: "survey-text",
-    preamble: "\n\n\n\n\n" + stimArray[i]["word1"],
+    preamble: "\n\n\n\n\n" + "<p id = 'cue-words'>" + stimArray[i]["word1"] + "</p>,
     questions: [
       {
         prompt: "",
